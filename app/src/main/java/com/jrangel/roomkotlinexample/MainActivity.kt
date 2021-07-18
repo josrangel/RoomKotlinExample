d53a1ad366346177dc728135b75f92294240e2a1
Josue <josrangel@hotmail.com>
2021-07-18 02:43:33 -0500

    - use the database for create a new user in MainActivity

--- app/src/main/java/com/jrangel/roomkotlinexample/MainActivity.kt
+++ app/src/main/java/com/jrangel/roomkotlinexample/MainActivity.kt
@@ -2,10 +2,32 @@ package com.jrangel.roomkotlinexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jrangel.roomkotlinexample.database.UserRepository
+import com.jrangel.roomkotlinexample.entity.User
+import org.jetbrains.anko.doAsync
+
+/**
+ * Tutoriales
+ *
+ * Room
+ * https://developer.android.com/codelabs/android-room-with-a-view-kotlin?hl=es-419
+ *
+ * Anko
+ * https://github.com/Kotlin/anko
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val db = UserRepository().dbInit(baseContext)
+        val user = User()
+        user.firstName = "memin"
+        user.lastName = "canallin"
+        user.age = 25
+        doAsync {
+            db.userDao().insert(user)
+        }
+
     }
 }
