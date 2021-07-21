a5d4738859a3453c8a8e0a2c6905f5582c8f55bf
Josue <josrangel@hotmail.com>
2021-07-20 23:14:37 -0500

    - add addNewUser function

--- app/src/main/java/com/jrangel/roomkotlinexample/MainActivity.kt
+++ app/src/main/java/com/jrangel/roomkotlinexample/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.jrangel.roomkotlinexample
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.jrangel.roomkotlinexample.database.DatabaseRoom
 import com.jrangel.roomkotlinexample.database.UserRepository
 import com.jrangel.roomkotlinexample.entity.User
 import org.jetbrains.anko.doAsync
@@ -17,17 +20,30 @@ import org.jetbrains.anko.doAsync
  */
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var etFirstName: EditText
+    lateinit var etLastName: EditText
+    lateinit var etAge: EditText
+    lateinit var db: DatabaseRoom
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val db = UserRepository().dbInit(baseContext)
+
+        db = UserRepository().dbInit(baseContext)
+
+        etFirstName = findViewById(R.id.et_first_name)
+        etLastName = findViewById(R.id.et_last_name)
+        etAge = findViewById(R.id.et_age)
+    }
+
+    fun addNewUser(view: View) {
         val user = User()
-        user.firstName = "memin"
-        user.lastName = "canallin"
-        user.age = 25
+        user.firstName = etFirstName.text.toString()
+        user.lastName = etLastName.text.toString()
+        user.age = etAge.text.toString().toInt()
         doAsync {
             db.userDao().insert(user)
         }
-
     }
 }
