7d1d19062d13af8c463361c00a82bdf36b596172
Josue <josrangel@hotmail.com>
2021-09-05 01:56:20 -0500

    - add registerForActivityResult - add update methods

--- app/src/main/java/com/jrangel/roomkotlinexample/MainActivity.kt
+++ app/src/main/java/com/jrangel/roomkotlinexample/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.jrangel.roomkotlinexample
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +14,7 @@ import com.jrangel.roomkotlinexample.database.DatabaseRoom
 import com.jrangel.roomkotlinexample.database.UserRepository
 import com.jrangel.roomkotlinexample.entity.User
 import com.jrangel.roomkotlinexample.listeners.OnDeleteListener
+import com.jrangel.roomkotlinexample.listeners.OnSelectListener
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -24,15 +28,24 @@ import org.jetbrains.anko.uiThread
  * https://github.com/Kotlin/anko
  */
 
-class MainActivity : AppCompatActivity(), OnDeleteListener {
+class MainActivity : AppCompatActivity(), OnDeleteListener, OnSelectListener {
 
-    lateinit var etFirstName: EditText
-    lateinit var etLastName: EditText
-    lateinit var etAge: EditText
-    lateinit var db: DatabaseRoom
-    lateinit var rvUser: RecyclerView
-    lateinit var userAdapter: UserAdapter
-    lateinit var dataRV: ArrayList<User>
+    private lateinit var etFirstName: EditText
+    private lateinit var etLastName: EditText
+    private lateinit var etAge: EditText
+    private lateinit var db: DatabaseRoom
+    private lateinit var rvUser: RecyclerView
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var dataRV: ArrayList<User>
+    private lateinit var previewRequest: ActivityResultLauncher<Intent>
+
+    companion object {
+        const val EXTRA_ID = "id"
+        const val EXTRA_FIRST_NAME = "firstName"
+        const val EXTRA_LAST_NAME = "lastName"
+        const val EXTRA_AGE = "age"
+        const val EXTRA_POSITION = "position"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +59,20 @@ class MainActivity : AppCompatActivity(), OnDeleteListener {
         rvUser = findViewById(R.id.rv_user)
 
         chargeUsers()
+
+        previewRequest =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    val extras = it.data?.extras
+                    val user = User()
+                    user.id = extras?.getInt(EXTRA_ID)!!
+                    user.firstName = extras?.getString(EXTRA_FIRST_NAME)!!
+                    user.lastName = extras?.getString(EXTRA_LAST_NAME)!!
+                    user.age = extras?.getInt(EXTRA_AGE)
+                    val position = extras?.getInt(EXTRA_POSITION)
+                    updateUser(user, position)
+                }
+            }
     }
 
     private fun initRecyclerView() {
@@ -68,13 +95,18 @@ class MainActivity : AppCompatActivity(), OnDeleteListener {
 
     private fun notifyNewUser() {
         userAdapter.notifyItemInserted(0)
-        userAdapter.notifyItemRangeChanged(0, userAdapter.itemCount);
+        userAdapter.notifyItemRangeChanged(0, userAdapter.itemCount)
         rvUser.scrollToPosition(0)
     }
 
     private fun notifyDeleteUser(position: Int) {
         userAdapter.notifyItemRemoved(position)
-        userAdapter.notifyItemRangeChanged(position, userAdapter.itemCount);
+        userAdapter.notifyItemRangeChanged(position, userAdapter.itemCount)
+    }
+
+    private fun notifyChangeUser(position: Int) {
+        userAdapter.notifyItemChanged(position)
+        rvUser.scrollToPosition(position)
     }
 
     fun addNewUser(view: View) {
@@ -99,5 +131,33 @@ class MainActivity : AppCompatActivity(), OnDeleteListener {
                 notifyDeleteUser(position)
             }
         }
+    }
+
+    private fun updateUser(user: User, position: Int) {
+        doAsync {
+            db.userDao().update(user)
+            dataRV[position] = user
+            uiThread {
+                notifyChangeUser(position)
+            }
+        }
+    }
+
+    override fun openUpdateActivity(
+        id: Int,
+        firstName: String,
+        lastName: String,
+        age: Int,
+        position: Int
+    ) {
+        val bundle = Bundle()
+        bundle.putInt(EXTRA_ID, id)
+        bundle.putString(EXTRA_FIRST_NAME, firstName)
+        bundle.putString(EXTRA_LAST_NAME, lastName)
+        bundle.putInt(EXTRA_AGE, age)
+        bundle.putInt(EXTRA_POSITION, position)
+        val intent = Intent(this, RegisterActivity::class.java)
+        intent.putExtras(bundle)
+        previewRequest.launch(intent)
     }
 }
