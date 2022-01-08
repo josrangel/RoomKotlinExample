ce8f38aa9a39f5baf66c0ec268f232fb3028ac52
josrangel <josrangel@hotmail.com>
2022-01-07 23:16:47 -0600

    - add return rxjava types in dao class

--- app/src/main/java/com/jrangel/roomkotlinexample/database/UserDAO.kt
+++ app/src/main/java/com/jrangel/roomkotlinexample/database/UserDAO.kt
@@ -2,22 +2,24 @@ package com.jrangel.roomkotlinexample.database
 
 import androidx.room.*
 import com.jrangel.roomkotlinexample.entity.User
+import io.reactivex.Completable
+import io.reactivex.Maybe
 
 @Dao
 interface UserDAO {
 
     @Query("SELECT * FROM user_table ORDER BY id DESC")
-    fun getAll(): List<User>
+    fun getAll(): Maybe<List<User>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(user: User)
+    fun insert(user: User): Completable
 
     @Update
-    fun update(user: User)
+    fun update(user: User): Completable
 
     @Delete
-    fun delete(user: User)
+    fun delete(user: User): Completable
 
     @Query("DELETE FROM user_table")
-    fun deleteAll()
+    fun deleteAll(): Completable
 }
